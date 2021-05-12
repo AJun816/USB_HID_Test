ebc7c141afa3589a42a3c35b29986fc31ca283c3
Jack Xu <Jack.Xu@qs-tec.com>
2021-05-12 11:33:20 +0800

    代码格式调整

--- USB_HID_Test/Program.cs
+++ USB_HID_Test/Program.cs
@@ -8,13 +8,13 @@ namespace USB_HID_Test
         static void Main(string[] args)
         {
             HidDevice hidDevice = new HidDevice();
-            hidDevice.Initial(0x0951, 0x16E4, "vid_0951&pid_16e4&mi_01&col05");        
+                    
             hidDevice.isConnectedFunc = new HidDevice.isConnectedDelegate(state =>
             {
                 if (state)
                 {
                     Console.WriteLine("连接成功");
-                    byte[] bytes = new byte[] { 0x07, 0x83, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00 };     
+                    byte[] bytes = new byte[] { 0x07, 0x91, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};     
                     
                     bool isSend = hidDevice.ReadFeatureData(bytes);
                     Thread.Sleep(10);
@@ -26,6 +26,7 @@ namespace USB_HID_Test
                     Console.WriteLine("连接失败");
                 }
             });
+            hidDevice.Initial(0x0951, 0x16E4, "vid_0951&pid_16e4&mi_01&col05");
             Console.Read();
         }
     }
